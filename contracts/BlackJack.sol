ba38349277718ff2cfb7460c9e07c8819105a2e1
dyduong <dylan.dng@berkeley.edu>
2017-03-09 01:26:11 -0800

    Update BlackJack.sol

--- contracts/BlackJack.sol
+++ contracts/BlackJack.sol
@@ -17,18 +17,18 @@ contract Blackjack {
 	uint numGames;
 	uint numPlayers;
 	function newGame(uint min_buy_in){
-         Game a = Games[numGames++];
-         a.timer_per_turn = block.number + 60 seconds; //changing time later
-         a.min_buy_in = min_buy_in;
-         a.seats = 8;
+            Game a = Games[numGames++];
+            a.timer_per_turn = block.number + 60 seconds; //changing time later
+            a.min_buy_in = min_buy_in;
+            a.seats = 8;
 	}
 	function newPlayer(uint numGames){
-		Game a = Games[numGame]
+	    Game a = Games[numGame]
 	    if(a.seats = 0) || (amount < a.min_buy_in){
 	        throw;
 	    }
 	    else{
-	        Player c = Players[numPlayers++];
+	    Player c = Players[numPlayers++];
             c.player = msg.sender;
             c.amount = msg.value;
             a.seats -= 1;
