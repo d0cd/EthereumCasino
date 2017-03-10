7388adecc2280b5780cb36ae8f0727b26404ab8b
Pranav Gaddamadugu <pranav@calvisitor-10-105-130-218.calvisitor.1918.berkeley.edu>
2017-03-09 16:40:34 -0800

    Created general structure for Casino contract

--- contracts/Casino.sol
+++ contracts/Casino.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.4;
 contract Casino {
   struct UserAccount {
     uint balance;
+    address ID;
     uint gamesWon;
     uint gamesLost;
     uint gamesTied;
@@ -10,14 +11,38 @@ contract Casino {
 
   mapping (address => UserAccount) accounts;
 
-  function Casino() {
 
+  function Casino() {}
+
+  function registerPlayer() payable {
+    if (accounts[msg.sender].ID != address(0x0)) {
+      accounts[msg.sender] = UserAccount(msg.value, msg.sender, 0,0,0);
+    } else {
+      throw;
+    }
   }
 
-  function registerPlayer() payable {} 
+  function createNewGame() payable {
+  }
 
-  function createGame() payable {}
+  function withdrawFunds() returns (bool) {
+    uint amtToSend = accounts[msg.sender].balance;
+    accounts[msg.sender].balance = 0;
+    if (msg.sender.send(amtToSend)) {
+      return true;
+    } else {
+      accounts[msg.sender].balance = amtToSend;
+      return false;
+    }
+  }
 
-  function withdrawFunds() {}
+  function depositFunds() payable {
+    if (accounts[msg.sender].ID != address(0x0)) {
+      accounts[msg.sender].balance += msg.value;
+    } else {
+      throw;
+    }
+
+  }
 
 }
